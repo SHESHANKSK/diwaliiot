a067f98bdaeeb1fcb647abbed56c60929e5e97b0
SHESHANKSK <sheshankkindalkar@gmail.com>
2021-12-11 11:05:42 +0530

    Updated Documentation

--- NODE_MCU_CODE/main/main.ino
+++ NODE_MCU_CODE/main/main.ino
@@ -8,36 +8,29 @@ const char *wifiName = "wifiname";
 const char *wifiPass = "password";
 WiFiClient wifiClient;
 
-int rgb_red = 5;
+int rgb_red = 12;
 int rgb_blue = 4;
-int rgb_green = 0;
+int rgb_green = 14;
 
-const char *host = " Enter the Side Address where you will deploy followed by / api";
-//Example
-//const char *host = "http://diwaliiot.herokuapp.com/api";
+//Set the API end point here
+//Example: const char *host = "http://diwaliiot.herokuapp.com/api";
+const char *host = "http://yourapiendpoint/api";
 
 void setup()
 {
 
   Serial.begin(115200);
   delay(10);
-  Serial.println();
-
-  Serial.print("Connecting to ");
-  Serial.println(wifiName);
-
   WiFi.begin(wifiName, wifiPass);
 
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("WiFi connection established");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP()); //You can get IP address assigned to ESP
+  Serial.println(WiFi.localIP());
 
   pinMode(rgb_red, OUTPUT);
   pinMode(rgb_blue, OUTPUT);
@@ -46,27 +39,21 @@ void setup()
 
 void loop()
 {
-  HTTPClient http; //Declare object of class HTTPClient
-  Serial.print("Request Link:");
-  Serial.println(host);
-  http.begin(wifiClient, host);      //Specify request destination
-  int httpCode = http.GET();         //Send the request
-  String payload = http.getString(); //Get the response payload from server
-  Serial.print("Response Code:");    //200 is OK
-  Serial.println(httpCode);          //Print HTTP return code
-  Serial.print("Returned data from Server:");
-  Serial.println(payload); //Print request response payload
+  HTTPClient http;
+  http.begin(wifiClient, host);
+  int httpCode = http.GET();
+  String payload = http.getString();
+  Serial.println(httpCode); //Print the Response gode from the server
+  Serial.println(payload);  //Print json data that we get from the API
   if (httpCode == 200)
   {
-    // Allocate JsonBuffer
-    // Use arduinojson.org/assistant to compute the capacity.
     const size_t capacity = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(3) + 60;
     DynamicJsonBuffer jsonBuffer(capacity);
     JsonObject &root = jsonBuffer.parseObject(payload);
 
     if (!root.success())
     {
-      Serial.println(F("Parsing failed!"));
+      Serial.println(F("Parsing Unsuccessful!"));
       return;
     }
 
@@ -75,13 +62,11 @@ void loop()
     int rgb_value_green_value = rgb_value["green_value"];
     int rgb_value_red_value = rgb_value["red_value"];
 
-    analogWrite(rgb_red, 255 - rgb_value_red_value);
-    analogWrite(rgb_green, 225 - rgb_value_green_value);
-    analogWrite(rgb_blue, 225 - rgb_value_blue_value);
+    analogWrite(rgb_red, rgb_value_red_value);
+    analogWrite(rgb_green, rgb_value_green_value);
+    analogWrite(rgb_blue, rgb_value_blue_value);
 
-    // Parse JSON object
-    // Decode JSON/Extract values
-    Serial.println(F("Response:"));
+    Serial.println(F("Response Received:"));
     Serial.println(rgb_value_blue_value);
     Serial.println(rgb_value_green_value);
     Serial.println(rgb_value_red_value);
@@ -89,10 +74,10 @@ void loop()
   }
   else
   {
-    Serial.println("Error in response");
+    Serial.println("Error");
   }
 
   http.end();
 
-  delay(500); //GET Data at every 0.5 seconds
+  delay(500); //GET Data after every 0.5 seconds
 }
--- README.md
+++ README.md
@@ -30,11 +30,80 @@ The following technologies are ued to accompalish this project
 | Slno | Components                        | Specifications               | Quantity            |
 | ---- | --------------------------------- | ---------------------------- | ------------------- |
 | 1    | Wifi Module Based Microcontroller | Node MCU                     | 1                   |
-| 2    | RGB Led                           | 5V (Common Cathode )         | 1                   |
+| 2    | RGB Led                           | 5V (Common Anode )           | 1                   |
 | 3    | Power Source                      | Battery (9V)                 | 1                   |
 | 4    | Resistors                         | 1.2 $K\Omega$                | 3                   |
 | 5    | Jumper Wires                      | Male to Male, Male to Female | As many as required |
 
-## Circuit Diagram
+## Circuit
 
-## Code Documentation
+![image](connection-diagram.jpg)
+
+## Pin Connection
+
+| Pin Name         | Pin Number   |
+| ---------------- | ------------ |
+| Red Pin          | D6 (GPIO 12) |
+| Green Pin        | D5 (GPIO 14) |
+| Blue Pin         | D2 (GPIO 4)  |
+| Comman Anode Pin | 3V3          |
+
+## Set up
+
+```cmd
+git clone https://github.com/SHESHANKSK/diwaliiot.git
+cd diwaliiot
+
+```
+
+```cmd
+pip install mkvirtualenv
+```
+
+```cmd
+mkvirtualenv <name of virtual environment>
+workon <name of virtual environment>
+```
+
+```cmd
+pip install -r requirements.txt
+```
+
+```cmd
+flask run
+OR
+python app.py
+```
+
+## Wifi and Endpoint Configurations
+
+main.ino file
+
+Change the name of your wifi and password
+
+```cpp
+const char *wifiName = "wifiname";
+const char *wifiPass = "password";
+```
+
+Deploy your flask app and get get the api link and change
+
+```cpp
+const char *host = "write the endpoint where your api is exposed";
+```
+
+## Deployment
+
+**Platform Used** : Heroku
+
+```cmd
+heroku login
+heroku create <name of your app>
+git push heroku master
+```
+
+## Reference
+
+Front-end Design : [Coding Artist Web](https://codingartistweb.com/2021/11/diya-css-animation-diwali-special/)
+
+Deployment: [Push Flask Apps To Heroku For Webhosting - Python and Flask #11](https://youtu.be/Li0Abz-KT78)
